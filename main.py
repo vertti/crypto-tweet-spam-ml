a107eba10d887ae9a3567ab893feca388252150b
Janne Sinivirta <janne.sinivirta@gmail.com>
2020-02-03 17:41:46 +0200

    remove lines with unknown result

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import json
 from pandas.io.json import json_normalize
 
 def load_set(name):
-  non_needed_lines = ['_input_hash', '_task_hash', 'label',
+  non_needed_lines = ['text', '_input_hash', '_task_hash', 'label',
                       'score', 'meta.user_id', 'meta.score', 'spans', 'priority']
   df = json_normalize(pd.Series(
       open(f'twitter-spam-model/{name}.jsonl').readlines()).apply(json.loads))
+  df = df[df.answer != 'ignore'] # drop lines with unknown result
   return df.drop(columns=non_needed_lines)
 
 training_df = load_set('training')
