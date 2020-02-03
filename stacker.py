a8cb121ffce9cbdf48016f664363ffbe5bd9d097
Janne Sinivirta <janne.sinivirta@gmail.com>
2020-02-03 20:24:31 +0200

    try to create StackingClassifier with prodigy model and catboost model

--- stacker.py
+++ stacker.py
@@ -1,7 +1,24 @@
 import spacy
 from catboost import CatBoostClassifier
+from sklearn.ensemble import StackingClassifier
+from loader import load_set
 
 prodigy_model = spacy.load('twitter-spam-model')
 
 catboost_model = CatBoostClassifier()
 catboost_model.load_model('catboost-model')
+
+clf = StackingClassifier(
+    estimators=[('prodigy', prodigy_model), ('catboost', catboost_model)],
+)
+
+training_df = load_set('training')
+evaluation_df = load_set('evaluation')
+
+X_train = training_df.drop(columns=['answer'])
+y_train = training_df.answer
+
+X_val = evaluation_df.drop(columns=['answer'])
+y_val = evaluation_df.answer
+
+print(clf.fit(X_train, y_train).score(X_val, y_val))
